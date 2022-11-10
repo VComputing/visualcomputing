fc031542bbf488ac7f8470e1498674959818c738
usuario <usuario@BE453S224S08ING.unal.loc>
2022-11-10 11:01:26 -0500

    blend

--- content/sketches/Shaders/Blending/sketch.js
+++ content/sketches/Shaders/Blending/sketch.js
@@ -4,29 +4,33 @@ let c1, c2;
 
 function preload(){
   blendShader = readShader('shaders/blend.frag',
-                          { matrices: Tree.NONE, varyings: Tree.color4 });
+                          { matrices: Tree.NONE, varyings: Tree.NONE });
 }
 
 function setup() {
   createCanvas(400, 400, WEBGL);
-  c1 = createColorPicker(color(255, 0, 0, 0));
+  colorMode(RGB, 1);
+  c1 = createColorPicker(color(1, 1, 0, 0));
   c1.position(0, 0);
-  c2 = createColorPicker(color(0, 255, 0, 0));
+  c2 = createColorPicker(color(0, 1, 1, 0));
   c2.position(width / 2, 0);
-  shader(blendShader)
+  //shader(blendShader)
 }
 
 function draw() {
   background(0);
-  uMaterial1 = c1.color();
-  uMaterial2 = c2.color();
-  fill(uMaterial1);
+  resetShader();
+  u1 = c1.color();
+  u2 = c2.color();
+  fill(u1);
   square(-width /2, -height/2 + 20, width / 2);
-  fill(uMaterial2);
+  resetShader();
+  fill(u2);
   square(0, -height/2 + 20, width / 2);
-  
-  blendShader.setUniform('uMaterial1', [255, 0, 0, 0]);
-  blendShader.setUniform('uMaterial2', [0, 255, 0, 0]);
   //shader(blendShader);
-  rect(-width/2, 10, width, height/2)
+  blendShader.setUniform('uMaterial1', [red(u1), green(u1), blue(u1), 0]);
+  blendShader.setUniform('uMaterial2', [red(u2), green(u2), blue(u2), 0]);
+  
+  //rect(0, height / 2, width, height);
+  circle(0, 60, 50);
 }
