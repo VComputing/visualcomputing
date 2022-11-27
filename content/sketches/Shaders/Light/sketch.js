39d16ab5709b7a5a74379211fd6aadf42bcd3212
Diego Fernando Bulla Poveda <48418033+dibullap@users.noreply.github.com>
2022-11-27 04:23:30 -0500

    Implementada la luz difusa, falta la especular y hacer que sea más configurable, se espera lograr esto mañana

--- content/sketches/Shaders/Light/sketch.js
+++ content/sketches/Shaders/Light/sketch.js
@@ -1,28 +1,43 @@
-let ambient;
+let c;
+let ambient, difuse, specular;
 let x, y, z, vx, vy, vz;
 let aLvl;
 
 function preload(){
   ambient = readShader('./shaders/ambient.frag', 
                         {varyings: Tree.NONE });
+  difuse = readShader('./shaders/difuse.frag',
+                        {varyings: Tree.normal3 | Tree.position4});
 }
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(1000, 500, WEBGL);
   angleMode(DEGREES);
   colorMode(RGB, 1);
   noLights();
+  
   aLvl = createSlider(0, 1, 0.5, 0.05);
   aLvl.style('width', '80px');
-  aLvl.input(() => { ambient.setUniform('ambient', aLvl.value()) });
+  aLvl.input(() => { 
+    ambient.setUniform('ambient', aLvl.value());
+    difuse.setUniform('ambient', aLvl.value()); 
+  });
   shader(ambient);
   ambient.setUniform('ambient', aLvl.value());
+  difuse.setUniform('ambient', aLvl.value());
+  
+  c = createCamera();
+  
+  c.camera(0, -100, 0);
+  //c.lookAt(0, 0, 1);
+  //c.frustum();
+
   x = 0;
   y = 0;
   z = 0;
   vx = 1;
-  vy = -1;
-  vz = 2;
+  vy = 1;
+  vz = 1;
 }
 
 function ejes(l){
@@ -37,40 +52,69 @@ function draw() {
   orbitControl();
   resetShader();
   push();
-    ejes(100);
-  pop();
-  
-  shader(ambient);
-  
-  push();
-    noStroke();
-    fill(0.5);
-    rotateX(90);
-    translate(0, 10, 0);
-    plane(200, 200);
-  pop();
-  
-  push();
-    noStroke();
-    fill(0,1,0);  
-    translate(x, y, z);
-    
+    fill(1,0,1, 0.1);  
+    translate(x, y, z)
     
     rotateX(x);
     rotateY(y);
     rotateZ(z);
-    //ejes(20);
-    //shader(ambient);  
+     
     sphere(5);
-    push();
+    /*push();
       ejes(20);
-    pop();
-    x += vx;
-    y += vy;
-    z += vz;
-    vx = (Math.abs(x) >= 100)? -vx: vx;
-    vy = (y <= -100 || y >= 0)? -vy: vy;
-    vz = (Math.abs(z) >= 100)? -vz: vz;
-    
+    pop();*/
+    x = Math.sin(frameCount * 0.01) * 90;
+    y = -Math.cos(frameCount * 0.01) - 90;
+    z = Math.cos(frameCount * 0.03) * 90;
+    vx = (Math.abs(x) >= 90)? -vx: vx;
+    vy = (y <= -90 || y >= 0)? -vy: vy;
+    vz = (Math.abs(z) >= 90)? -vz: vz;
+    c.lookAt(x, y, z);
   pop();
+
+  noStroke();
+  //shader(ambient);
+  shader(difuse);
+  difuse.setUniform('uLightPosition', treeLocation(createVector(x, y, z), 
+  { from: Tree.WORLD, to: Tree.EYE }).array());
+
+  push();
+    fill(1, 0, 0, 0.8);
+    translate(-100, -100, 0);
+    rotateY(90);
+    plane(200, 200);
+  pop();
+  push();
+    fill(0, 0, 1, 0.8);
+    translate(100, -100, 0);
+    rotateY(270);
+    plane(200, 200);
+  pop();
+  push();
+    fill(0, 0, 1, 0.8);
+    translate(0, -100, -100);
+    plane(200, 200);
+  pop();
+  
+  push();
+    fill(1, 0, 0, 0.8);
+    translate(0, -100, 100);
+    rotateY(180);
+    plane(200, 200);
+  pop();
+  push();
+    fill(1, 0, 0, 0.8);
+    rotateX(90);
+    plane(200, 200);
+  pop();
+  
+  push();
+    //shader(difuse);
+    fill(0, 0, 1, 0.8);
+    rotateX(270);
+    translate(0, 0, -200);
+    plane(200, 200);
+  pop();
+  
+  
 }
